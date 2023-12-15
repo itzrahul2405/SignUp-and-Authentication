542469745757b0a1938b325503a0b50898c4a63d
RahulSharma <rahulmlc2003@gmail.com>
2023-12-15 15:35:12 +0530

    login mechanism using firebase rest api

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -23,33 +23,39 @@ const AuthForm = () => {
     // Optional: Add Validation
 
     setIsLoading(true)
+    let url;
     if(isLogin){
-
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCUE_h406_9DStuM07UQUM4y-lfv0SidUU'
     }
     else{
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCUE_h406_9DStuM07UQUM4y-lfv0SidUU', {
-        method: 'POST',
-        body: JSON.stringify({email: enteredEmail, password: enteredPassword, returnSecureToken: true}),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(res => {
-        setIsLoading(false)
-        if(res.ok){
-          // ...
-        }
-        else{
-          return res.json().then(data => {           // res.json() also returns a promise
-            let errorMessage = 'Authentication failed!'
-            if(data && data.error && data.error.message){
-              errorMessage = data.error.message;
-            }
-            alert(errorMessage)
-          })
-        }
-      })
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCUE_h406_9DStuM07UQUM4y-lfv0SidUU'
     }
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({email: enteredEmail, password: enteredPassword, returnSecureToken: true}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      setIsLoading(false)
+      if(res.ok){
+        return res.json();
+      }
+      else{
+        return res.json().then(data => {           // res.json() also returns a promise
+          let errorMessage = 'Authentication failed!'
+          if(data && data.error && data.error.message){
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        })
+      }
+    })
+    .then(data => { console.log(data) })
+    .catch(err => { alert(err.message) })
+    
   }
 
 
